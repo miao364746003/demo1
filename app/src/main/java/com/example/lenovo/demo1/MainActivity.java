d22201c6fc896c04e4ae6da669d0e2ff672c616c
Tom <364746003@qq.com>
2018-07-05 20:41:02 +0800

    OnResume

--- app/src/main/java/com/example/lenovo/demo1/MainActivity.java
+++ app/src/main/java/com/example/lenovo/demo1/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.lenovo.demo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/*生命周期---获得焦点*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
